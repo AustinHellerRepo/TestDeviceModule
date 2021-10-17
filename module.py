ee3672960e80185d8d9ac1f16046b71814119789
austin <galaky2005@gmail.com>
2021-10-17 13:33:08 -0400

    started using DeviceModule repo; pushing device-specific information into constructor as per Module implementation; using updated send_message format;

--- module.py
+++ module.py
@@ -1,19 +1,26 @@
-from austin_heller_repo.socket import Module, start_thread, time
+from austin_heller_repo.socket import start_thread, time, json
+from austin_heller_repo.module import Module, ModuleMessage
 
 
 class ImplementedModule(Module):
 
-	def __init__(self):
-		super().__init__()
+	def __init__(self, *, device_guid: str, device_instance_guid: str, send_message_method, get_devices_by_purpose_method):
+		super().__init__(
+			device_guid=device_guid,
+			device_instance_guid=device_instance_guid,
+			send_message_method=send_message_method,
+			get_devices_by_purpose_method=get_devices_by_purpose_method
+		)
 
 		print("__init__")
 
 		self.__is_module_thread_running = False
 		self.__module_thread = None
 
-	def receive(self, *, data: str):
+	def receive(self, *, module_message: ModuleMessage):
 
-		print("received: \"" + data + "\"")
+		_json_string = json.dumps(module_message.get_transmission_json())
+		print("received: \"" + _json_string + "\"")
 
 	def start(self):
 
@@ -30,13 +37,51 @@ class ImplementedModule(Module):
 
 	def __thread_method(self):
 
-		self.get_send_method()("started")
+		_queue_guid = "D5195218-61F6-4EDF-8675-ABB279BA92CD"
+
+		self._send(
+			module_message=ModuleMessage(
+				queue_guid=_queue_guid,
+				transmission_json={
+					"message": "started"
+				},
+				source_device_guid=self._get_device_guid(),
+				source_device_instance_guid=self._get_device_instance_guid(),
+				destination_device_guid=self._get_device_guid(),
+				destination_device_instance_guid=self._get_device_instance_guid()
+			)
+		)
+
 		_iteration = 0
 		while self.__is_module_thread_running:
 			time.sleep(1.0)
 			_message = str(_iteration)
 			print("sending: \"" + _message + "\"")
-			self.get_send_method()(_message)
-			_iteration += 1
-		self.get_send_method()("ended")
 
+			self._send(
+				module_message=ModuleMessage(
+					queue_guid=_queue_guid,
+					transmission_json={
+						"message": _message
+					},
+					source_device_guid=self._get_device_guid(),
+					source_device_instance_guid=self._get_device_instance_guid(),
+					destination_device_guid=self._get_device_guid(),
+					destination_device_instance_guid=self._get_device_instance_guid()
+				)
+			)
+
+			_iteration += 1
+
+		self._send(
+			module_message=ModuleMessage(
+				queue_guid=_queue_guid,
+				transmission_json={
+					"message": "ended"
+				},
+				source_device_guid=self._get_device_guid(),
+				source_device_instance_guid=self._get_device_instance_guid(),
+				destination_device_guid=self._get_device_guid(),
+				destination_device_instance_guid=self._get_device_instance_guid()
+			)
+		)
